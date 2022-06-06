ab82c750f0bc66495ca3a50c8c25b66ff67c2db7
Alex Natalchyshyn <46841326+DevilMayNotCry@users.noreply.github.com>
2022-06-06 18:00:34 +0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from transformers import TFViTModel, ViTConfig, TFViTForImageClassification
+
+configuration = ViTConfig(hidden_size = 128,
+                          num_hidden_layers = 8,
+                          num_attention_heads = 8,
+                          intermediate_size = 128,
+                          hidden_act = "gelu")
+
+base_model = TFViTForImageClassification(configuration)
+
+pixel_values = tf.keras.layers.Input(shape=(3,224,224), name='pixel_values', dtype='float32')
+
+vit = base_model.vit(pixel_values)[0]
+x = tf.keras.layers.Dense(64, activation='relu')(vit[:, 0, :])
+classifier = tf.keras.layers.Dense(6, activation='softmax', name='outputs')(x)
+
+keras_model = tf.keras.Model(inputs=pixel_values, outputs=classifier)
